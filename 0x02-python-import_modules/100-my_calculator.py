0db27dc7338c1ad363e78fadd2d7039cb182ad16
dwkwan <dwkwan1989@gmail.com>
2019-05-02 20:38:22 +0000

    Fix string for multiply statement

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,13 +6,17 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if argv[2] == "+":
-        print("{:s} + {:s} = {:d}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
+        print("{:s} + {:s} = {:d}"
+              .format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
     elif argv[2] == "-":
-        print("{:s} - {:s} = {:d}".format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+        print("{:s} - {:s} = {:d}"
+              .format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
     elif argv[2] == "*":
-        print("{:s} - {:s} = {:d}".format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+        print("{:s} * {:s} = {:d}"
+              .format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
     elif argv[2] == "/":
-        print("{:s} / {:s} = {:d}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+        print("{:s} / {:s} = {:d}"
+              .format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
