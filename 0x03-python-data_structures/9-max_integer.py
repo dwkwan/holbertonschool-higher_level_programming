91a182f3bcc1f12eaa2aa7f17bdf900dd27fd943
dwkwan <dwkwan1989@gmail.com>
2019-05-07 20:30:14 +0000

    Add 9-max_integer.py task

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    result = my_list[0]
-    for i in my_list[1:]:
-        if i > result:
-            result = i
-    return result
+    if my_list:
+        result = my_list[0]
+        for i in my_list[1:]:
+            if i > result:
+                result = i
+        return result
