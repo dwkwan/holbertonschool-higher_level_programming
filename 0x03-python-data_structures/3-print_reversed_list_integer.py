3dc86f434b05e6d0f18be49cc42329afb6ece16f
dwkwan <dwkwan1989@gmail.com>
2019-05-07 19:38:29 +0000

    Add if statement to not reverse list if it's empty. Use proper formatting

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list != "":
+    if my_list != []:
         reversedlist = reversed(my_list)
         for i in reversedlist:
             print("{:d}".format(i))
