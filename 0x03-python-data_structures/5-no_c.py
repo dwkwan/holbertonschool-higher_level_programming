fbd8a47270498576264390f23a3023b6380ba1aa
dwkwan <dwkwan1989@gmail.com>
2019-05-08 20:55:53 +0000

    Remove assignment statement

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,4 @@ def no_c(my_string):
     for i in my_string:
         if i != 'c' and i != 'C':
             my_newstring += i
-    my_string = my_newstring
-    return my_string
+    return my_newstring
