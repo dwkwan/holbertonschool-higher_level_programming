949b5fb6716d50ae72e45fcf6a892bbdad919133
dwkwan <dwkwan1989@gmail.com>
2019-05-08 01:59:31 +0000

    Add 13-is_palindrome.c task

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * is_palindrome - check if listint_t linked list is a palindrome
+ * @head: a pointer to a pointer to a struct
+ *
+ * Return: 1 if palindrome, else 0
+ */
+listint_t *reverse_list(listint_t **head);
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head, *fast = *head;
+	int palindrome = 1;
+
+	if (*head)
+		return (1);
+	while (fast->next && fast->next->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	if (!fast->next->next)
+		slow = slow->next;
+	slow = reverse_list(&slow);
+	fast = *head;
+	while (slow)
+	{
+		if (slow->n != fast->n)
+			palindrome = 0;
+		slow = slow->next;
+		fast = fast->next;
+	}
+	if (palindrome)
+		return (1);
+	else
+		return (0);
+
+
+}
+/**
+ * reverse_list - reverses a listint_t linked list
+ * @head: a pointer to a pointer to a struct
+ *
+ * Return: a pointer to first node of reversed list
+ */
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL, *ahead = NULL;
+
+	while (*head)
+	{
+		ahead = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = ahead;
+	}
+	*head = prev;
+	return (*head);
+}
