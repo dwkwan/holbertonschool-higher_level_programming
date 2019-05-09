31c6d3a487377b9c357269f2bc7def5ec7e6f5d6
dwkwan <dwkwan1989@gmail.com>
2019-05-09 19:03:06 +0000

    Remove unnecessary if statement

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
         return [replace if i == search else i for i in my_list]
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
         new_list = []
         for i in my_list:
             if i not in new_list:
