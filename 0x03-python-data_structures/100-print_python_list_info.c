11b8ac7abd5526ca767b8b17e8507927b0429787
dwkwan <dwkwan1989@gmail.com>
2019-05-09 03:32:19 +0000

    Modify to print allocation and item types

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -5,8 +5,13 @@
  */
 void print_python_list_info(PyObject *p)
 {
+	size_t i = 0;
 	size_t length = 0;
+	PyListObject *Plist = (PyListObject *)p;
 
 	length = PyList_Size(p);
 	printf("[*] Size of the Python List = %lu\n", length);
+	printf("[*] Allocated = %lu\n", Plist->allocated);
+	for (i = 0; i < length; i++)
+		printf("Element %lu: %s\n", i, Py_TYPE(Plist->ob_item[i])->tp_name);
 }
