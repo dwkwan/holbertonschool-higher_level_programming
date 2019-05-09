8018bd09375c1c78b30c3a7500e7a6b77fba2477
dwkwan <dwkwan1989@gmail.com>
2019-05-09 22:05:17 +0000

    Add task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,6 @@ def best_score(a_dictionary):
     if a_dictionary:
         for key in a_dictionary:
             if a_dictionary[key] > result:
-                a_dictionary[key] = result
+                result = a_dictionary[key]
                 winner = key
         return winner
