92639d2bee736f05f094ac2da32d8e7463043f27
dwkwan <dwkwan1989@gmail.com>
2019-05-09 18:47:54 +0000

    Remove whitespace before '('

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for k in sorted(a_dictionary):
-        print ("{}: {}".format(k, a_dictionary[k]))
+        print("{}: {}".format(k, a_dictionary[k]))
