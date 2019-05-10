22c8faa634046a9fb569b80fee6d20332e9e8590
dwkwan <dwkwan1989@gmail.com>
2019-05-10 03:18:06 +0000

    Add task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    total = 0
+    divisor = 0
+    for x in my_list:
+        total += x[0] * x[1]
+        divisor += x[1]
+    return total/divisor
