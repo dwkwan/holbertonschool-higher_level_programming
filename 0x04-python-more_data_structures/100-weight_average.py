e92a7ea2c4154f26ca687e7c83f7f290f15e8b0d
dwkwan <dwkwan1989@gmail.com>
2019-05-10 03:23:49 +0000

    Add return for empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    total = 0
-    divisor = 0
-    for x in my_list:
-        total += x[0] * x[1]
-        divisor += x[1]
-    return total/divisor
+    if my_list:
+        total = 0
+        divisor = 0
+        for x in my_list:
+            total += x[0] * x[1]
+            divisor += x[1]
+        return total/divisor
+    return 0
