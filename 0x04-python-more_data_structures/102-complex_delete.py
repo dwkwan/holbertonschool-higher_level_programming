fb80d7fdf9b8fc050c36feb1249682af9802e661
dwkwan <dwkwan1989@gmail.com>
2019-05-13 04:39:19 +0000

    Refactor to handle more cases of key deletion

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for k, v in a_dictionary.items():
-        if v == value:
+    copy = a_dictionary.copy()
+    for k in copy:
+        if copy[k] == value:
             del a_dictionary[k]
-            break
-    for k, v in a_dictionary.items():
-        if v == value:
-            del a_dictionary[k]
-            break
-    for k, v in a_dictionary.items():
-        if v == value:
-            del a_dictionary[k]
-            break
     return a_dictionary
