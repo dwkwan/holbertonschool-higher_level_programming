f08747e2ce2891cce7560945e38e3ddd0fb57f77
David Kwan <dwkwan1989@gmail.com>
2019-05-20 18:59:29 +0000

    Handle all exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as e:
+    except Exception as e:
         print("Exception:", e, file=sys.stderr)
         return False
