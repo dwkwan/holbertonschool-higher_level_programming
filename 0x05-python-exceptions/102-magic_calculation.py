4af242d3b7302992911e85a081de72a0ee2799af
David Kwan <dwkwan1989@gmail.com>
2019-05-20 22:13:01 +0000

    Add 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -5,8 +5,11 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception("Too far")
+            else:
                 result += (a ** b)/i
         except:
             result = b + a
             break
     return (result)
+import dis
+dis.dis(magic_calculation)
