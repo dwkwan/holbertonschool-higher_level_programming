3dba75873df1c29ea1ced12df7d3a455f2968378
David Kwan <dwkwan1989@gmail.com>
2019-05-20 18:55:37 +0000

    Return immediately

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ def safe_print_integer_err(value):
     import sys
     try:
         print("{:d}".format(value))
+        return True
     except ValueError as e:
         print("Exception:", e, file=sys.stderr)
         return False
-    return True
