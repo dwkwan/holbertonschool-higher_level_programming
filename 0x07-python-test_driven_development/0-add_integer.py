b896655942f902187ede5c481e93c4cd9c6de118
David Kwan <dwkwan1989@gmail.com>
2019-05-25 05:12:46 +0000

    Don't allow nan for addend

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,8 +11,8 @@ def add_integer(a, b=98):
     Returns:
         int: The return value. The sum
     """
-    if type(a) != int and type(a) != float:
+    if (type(a) != int and type(a) != float) or a != a:
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
+    if type(b) != int and type(b) != float or b != b:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
