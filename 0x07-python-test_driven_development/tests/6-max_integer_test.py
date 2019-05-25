489e332ef9f1f540da1890b6b5fdae884b7f2c57
David Kwan <dwkwan1989@gmail.com>
2019-05-25 04:15:54 +0000

    Remove list initializations

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,32 +8,25 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """This function tests for positive output"""
     def test_positiveoutput(self):
-        list = [1, 2, 3, 4]
-        self.assertEqual(max_integer(list), 4)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
     """This function tests for negative output"""
     def test_max_negativeoutput(self):
-        list = [-1, -2, -3, -4]
-        self.assertEqual(max_integer(list), -1)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
     """This function tests for one number"""
     def test_max_onenumber(self):
-        list = [3]
-        self.assertEqual(max_integer(list), 3)
+        self.assertEqual(max_integer([3]), 3)
     """This function tests for one negative number"""
     def test_max_onenegativenumber(self):
-        list = [-1]
-        self.assertEqual(max_integer(list), -1)
+        self.assertEqual(max_integer([-1]), -1)
     """This function tests for unordered list"""
     def test_max_unordered(self):
-        list = [1, 4, 2, 3]
-        self.assertEqual(max_integer(list), 4)
+        self.assertEqual(max_integer([1, 4, 2, 3]), 4)
     """This function tests for identical values"""
     def test_max_identicalvalues(self):
-        list = [3, 3, 3, 3]
-        self.assertEqual(max_integer(list), 3)
+        self.assertEqual(max_integer([3, 3, 3, 3]), 3)
     """This function tests for strings"""
     def test_max_strings(self):
-        list = ['a', 'b', 'c', 'd']
-        self.assertEqual(max_integer(list), 'd')
+        self.assertEqual(max_integer(['a', 'b', 'c', 'd']), 'd')
     """This function tests for raising error when None is passed"""
     def test_max_none(self):
         with self.assertRaises(TypeError):
