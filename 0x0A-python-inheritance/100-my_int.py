8e39a5ed5274c28a232d36c8191f34d177538d21
David Kwan <dwkwan1989@gmail.com>
2019-06-06 06:09:52 +0000

    Add advanced task

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module creates a class named MyInt
+"""
+
+
+class MyInt(int):
+    """
+    A class named MyInt
+    """
+
+    def __eq__(self, other):
+        """Swaps the eq builtin"""
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        """Swaps the eq builtin"""
+        return int.__eq__(self, other)
