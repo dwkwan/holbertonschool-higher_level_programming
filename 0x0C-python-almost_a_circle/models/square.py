bd5125fec21285abfac3a9f1496d76ad1d9b6c17
David Kwan <dwkwan1989@gmail.com>
2019-06-09 19:15:30 +0000

    Update comments

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,8 +16,9 @@ class Square(Rectangle):
     attr5(y): number of spaces before square
     """
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializes instance of Rectangle class"""
+        """Initializes instance of Square class"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """Returns string representation of Square class"""
               return"[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x, self.y, self.width, self.height)
