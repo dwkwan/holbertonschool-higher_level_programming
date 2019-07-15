ec99d3356d0d43741c3fa0ebbae35837a98e9d09
David Kwan <dwkwan1989@gmail.com>
2019-07-15 20:37:54 +0000

    Add task

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- the number of records with the same score in the table second_table
+-- lists the number of records with the same score in the table second_table
+SELECT score, count(*) as number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC
