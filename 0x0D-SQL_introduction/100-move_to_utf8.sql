545fd0e22cc6c1405fefd96fa9cc3c99e1c11020
David Kwan <dwkwan1989@gmail.com>
2019-07-15 21:18:28 +0000

    Add task

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- hbtn_0c_0 converted to UTF8
+-- converts hbtn_0c_0 converted to UTF8
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
