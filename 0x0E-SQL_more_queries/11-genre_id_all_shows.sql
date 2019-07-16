0409343b74c0a1c3bbbd1d1b532616bf161439bc
David Kwan <dwkwan1989@gmail.com>
2019-07-16 22:01:35 +0000

    Update comments

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
---  all shows contained in hbtn_0d_tvshows that have at least one genre linked
--- lists  all shows contained in hbtn_0d_tvshows that have at least one genre linked
+--  all shows contained in hbtn_0d_tvshows
+-- lists all shows contained in hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
