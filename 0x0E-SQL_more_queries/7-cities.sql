28f0b7aa9c8a2999ed3d9b9cba05d5f5ac8f58a0
David Kwan <dwkwan1989@gmail.com>
2019-07-16 19:38:30 +0000

    Add USE keyword statement

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,8 @@
 -- The database hbtn_0d_usa and table cities created on MySQL server
 -- Creates the database hbtn_0d_usa and table cities created on MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
 id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
