0bfc4836ba95569839761fe3e5b56182c47891ec
David Kwan <dwkwan1989@gmail.com>
2019-07-16 21:20:19 +0000

    Add task

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+--  all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- lists  all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+RIGHT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
