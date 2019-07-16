2d3d659a056f1a7b0d7f9a91a07fe38e30ba9aea
David Kwan <dwkwan1989@gmail.com>
2019-07-16 23:49:29 +0000

    Change column name

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 --  all shows contained in hbtn_0d_tvshows with number of shows linked to each
 -- lists all shows contained in hbtn_0d_tvshows with number of shows linked to each
-SELECT tv_genres.name AS genres, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
