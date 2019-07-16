a466e82debd9a3e6001b713703a30dae3d399eea
David Kwan <dwkwan1989@gmail.com>
2019-07-16 19:50:36 +0000

    Use single quotes

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- All the cities of California that can be found in the database hbtn_0d_usa
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT
-       name, id
+       id, name
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = "California")
-ORDER BY id ASC
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
