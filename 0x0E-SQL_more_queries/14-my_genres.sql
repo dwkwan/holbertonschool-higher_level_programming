07a02bc359b0c1fb1cf39e177acf129dbe9d4376
David Kwan <dwkwan1989@gmail.com>
2019-07-17 00:22:04 +0000

    Use single quotes

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,4 +6,4 @@ INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = "Dexter"
+WHERE tv_shows.title = 'Dexter'
