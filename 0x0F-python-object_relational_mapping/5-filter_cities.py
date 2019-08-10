98909bcfb46cc717c837c990cf931a1afeb64198
David Kwan <dwkwan1989@gmail.com>
2019-08-10 19:02:01 +0000

    Refactor for pep

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    query = "SELECT cities.name FROM cities" +
-    " INNER JOIN states ON cities.state_id = states.id " +
-    " WHERE states.name = %s" + " ORDER BY" + " cities.id ASC"
+    query = "SELECT cities.name FROM cities"
+    query = query + " INNER JOIN states ON cities.state_id = states.id"
+    query = query + " WHERE states.name = %s ORDER BY cities.id ASC"
     cur.execute(query, [sys.argv[4]])
     query_rows = cur.fetchall()
     for row in range(len(query_rows) - 1):
