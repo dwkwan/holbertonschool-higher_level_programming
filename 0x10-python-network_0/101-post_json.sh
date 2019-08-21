8312e6e21a19a3b1c2455d20a65526a73e9bd315
David Kwan <dwkwan1989@gmail.com>
2019-08-21 00:58:23 +0000

    Add silent flag

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl "{$1}" -X POST -H "Content-Type: application/json" -d @$2
+curl -s "{$1}" -X POST -H "Content-Type: application/json" -d @$2
