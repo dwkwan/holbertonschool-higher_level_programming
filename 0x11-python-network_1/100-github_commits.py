b09edd5014338183f15144c2972ee33b514da890
David Kwan <dwkwan1989@gmail.com>
2019-08-26 23:12:45 +0000

    Modify argument order

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,13 +5,13 @@ API to display your id
 import requests
 from sys import argv
 
-owner = argv[1]
-repository_name = argv[2]
+repository_name = argv[1]
+owner_name = argv[2]
 
 if __name__ == "__main__":
     r = requests.get(
         'https://api.github.com/repos/{:}/{:}/commits?per_page=10'.format(
-            owner, repository_name)).json()
+            owner_name, repository_name)).json()
     for item in range(len(r)):
         print("{:}: {:}".format(r[item].get('sha'), r[item].get('commit').get(
             'committer').get('name')))
