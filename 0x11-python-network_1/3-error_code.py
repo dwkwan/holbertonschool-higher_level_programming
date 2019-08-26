67b26c98e3209f481631a46cd33ea79c4f1ad487
David Kwan <dwkwan1989@gmail.com>
2019-08-26 20:12:38 +0000

    Missing colon

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
             html = response.read()
             print(html.decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print("Error code {:}".format(e.code))
+        print("Error code: {:}".format(e.code))
