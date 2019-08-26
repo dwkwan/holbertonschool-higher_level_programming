08ae5b227faac900f25b60de706dcfd51d85fd2e
David Kwan <dwkwan1989@gmail.com>
2019-08-26 23:24:18 +0000

    Handle printing less than 10 commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     repository_name = argv[1]
     owner_name = argv[2]
     r = requests.get(
-        'https://api.github.com/repos/{:}/{:}/commits?per_page=10'.format(
+        'https://api.github.com/repos/{:}/{:}/commits'.format(
             owner_name, repository_name)).json()
     for item in range(len(r)):
-        print("{:}: {:}".format(r[item].get('sha'), r[item].get('commit').get(
-            'committer').get('name')))
+        if item < 10:
+            print("{:}: {:}".format(r[item].get('sha'), r[item].get(
+                'commit').get('committer').get('name')))
