299e5dbb3f25ed6a9209111827a477098ec58cec
David Kwan <dwkwan1989@gmail.com>
2019-09-17 20:01:59 +0000

    Modify output string

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 if (isNaN(parseInt(process.argv[2], 10))) {
   console.log('Not a number');
 } else {
-  console.log('My number is:', parseInt(process.argv[2], 10));
+  console.log('My number:', parseInt(process.argv[2], 10));
 }
