778c9cfccdd8a92468a77d6133b81b1fabec0e0b
David Kwan <dwkwan1989@gmail.com>
2019-09-17 20:43:31 +0000

    Sort array numerically

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,17 +3,6 @@ if (process.argv.length < 4) {
   console.log('0');
 } else {
   const mod = process.argv.slice(2);
-  let i;
-  let first = Number.MIN_VALUE;
-  let second = Number.MIN_VALUE;
-  for (i = 0; i < mod.length; i++) {
-    if (mod[i] > first) {
-      second = first;
-      first = mod[i];
-    }
-    if (first > mod[i] > second) {
-      second = mod[i];
-    }
-  }
-  console.log(second);
+  mod.sort((a, b) => b - a);
+  console.log(mod[1]);
 }
