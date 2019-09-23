e84eb3a32e814576746972ef969fb1492c40b0d0
David Kwan <dwkwan1989@gmail.com>
2019-09-23 18:48:24 +0000

    Add task 7 and 8

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let i = 0;
+  let count = 0;
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+exports.esrever = function (list) {
+  let i = 0;
+  let j = list.length - 1;
+  for (i = 0; i < list.length / 2; i++, j--) {
+    [list[i], list[j]] = [list[j], list[i]];
+  }
+  return list;
+};
