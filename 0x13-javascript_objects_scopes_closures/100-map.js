450c08ec984d54d62d6b334ec58e8dddb42b4e13
David Kwan <dwkwan1989@gmail.com>
2019-09-23 22:36:42 +0000

    Use call back function

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-const map1 = list.map(x => x * list.indexOf(x));
+const map1 = list.map((x, i) => { return x * i; });
 console.log(map1);
