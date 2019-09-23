8dd8c68aca054f6644ac32e96c3f144451f54d8f
David Kwan <dwkwan1989@gmail.com>
2019-09-23 21:06:50 +0000

    Align with semistandard

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 exports.logMe = (function (item) {
   let counter = -1;
-  return function (item) { counter += 1; console.log(counter + ': ' + item);};
+  return function (item) { counter += 1; console.log(counter + ': ' + item); };
 })();
