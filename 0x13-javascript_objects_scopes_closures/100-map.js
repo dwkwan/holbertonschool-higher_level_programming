ed8cbd605fd13b21ef5628f59967dfda52939418
David Kwan <dwkwan1989@gmail.com>
2019-09-23 22:26:56 +0000

    Save list to const

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-console.log(list.map(x => x * list.indexOf(x)));
+const map1 = list.map(x => x * list.indexOf(x));
+console.log(map1);
