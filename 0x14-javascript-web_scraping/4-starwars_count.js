050e32ab6846b81d36c853ef135ed211da584f3a
David Kwan <dwkwan1989@gmail.com>
2019-09-24 21:23:19 +0000

    Check for substring in list

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,12 +4,15 @@ const url = process.argv[2];
 let resultDict = {};
 let count = 0;
 let i = 0;
+let j = 0;
 request(url, function (
   response, body) {
   resultDict = JSON.parse(body.body);
   for (i = 0; i < resultDict.results.length; i++) {
-    if (resultDict.results[i].characters.includes('https://swapi.co/api/people/18/')) {
-      count++;
+    for (j = 0; j < resultDict.results[i].characters.length; j++) {
+      if (resultDict.results[i].characters[j].includes('/18/')) {
+        count++;
+      }
     }
   }
   console.log(count);
