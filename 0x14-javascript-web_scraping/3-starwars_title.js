4a7822d7974116e8f40019295fff7fa2f7ad2657
David Kwan <dwkwan1989@gmail.com>
2019-09-24 19:34:12 +0000

    Ahere to semistandard style

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,5 +4,5 @@ const url = 'http://swapi.co/api/films/' + process.argv[2];
 let resultDict = {};
 request(url, function (response, body) {
   resultDict = JSON.parse(body.body);
-  console.log(resultDict["title"]);
+  console.log(resultDict.title);
 });
