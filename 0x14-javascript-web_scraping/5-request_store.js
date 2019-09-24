ee027f40e1b13637e6c3346941605ce54370c31b
David Kwan <dwkwan1989@gmail.com>
2019-09-24 19:49:44 +0000

    Add task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+let text;
+request(url, function (response, body) {
+  text = body.body;
+  fs.writeFile(process.argv[3], text, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+  })
+  ;
+});
