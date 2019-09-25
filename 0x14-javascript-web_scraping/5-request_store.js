5d19cd463d779c3060c30909199af7704d1591e2
David Kwan <dwkwan1989@gmail.com>
2019-09-25 20:54:14 +0000

    If error, print error

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,11 +3,15 @@ const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 let text;
-request(url, function (response, b) {
-  text = b.body;
-  fs.writeFile(process.argv[3], text, 'utf-8', (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    text = body;
+    fs.writeFile(process.argv[3], text, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
 });
