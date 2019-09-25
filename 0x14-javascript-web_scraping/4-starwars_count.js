bce6be6f268b6188857d21a4d7f7c227f90b8189
David Kwan <dwkwan1989@gmail.com>
2019-09-25 20:45:27 +0000

    If error, print error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,14 +6,18 @@ let count = 0;
 let i = 0;
 let j = 0;
 request(url, function (
-  response, body) {
-  resultDict = JSON.parse(body.body);
-  for (i = 0; i < resultDict.results.length; i++) {
-    for (j = 0; j < resultDict.results[i].characters.length; j++) {
-      if (resultDict.results[i].characters[j].includes('/18/')) {
-        count++;
+  error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    resultDict = JSON.parse(body);
+    for (i = 0; i < resultDict.results.length; i++) {
+      for (j = 0; j < resultDict.results[i].characters.length; j++) {
+        if (resultDict.results[i].characters[j].includes('/18/')) {
+          count++;
+        }
       }
     }
+    console.log(count);
   }
-  console.log(count);
 });
