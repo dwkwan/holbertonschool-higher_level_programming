1435880a13fb3331643262ff78c03143c8373e4b
David Kwan <dwkwan1989@gmail.com>
2019-09-25 20:59:20 +0000

    If error, print error

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,12 +5,18 @@ let movieDict = {};
 let characterDict = {};
 let i = 0;
 request(url, function (
-  response, body) {
-  movieDict = JSON.parse(body.body);
-  for (i = 0; i < movieDict.characters.length; i++) {
-    request(movieDict.characters[i], function (request, body) {
-      characterDict = JSON.parse(body.body);
-      console.log(characterDict.name);
-    });
+  error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    movieDict = JSON.parse(body.body);
+    for (i = 0; i < movieDict.characters.length; i++) {
+      console.log(movieDict.characters[i]);
+      request(movieDict.characters[i], function (request, body) {
+        characterDict = JSON.parse(body.body);
+        console.log(characterDict);
+        console.log(characterDict.name);
+      });
+    }
   }
 });
